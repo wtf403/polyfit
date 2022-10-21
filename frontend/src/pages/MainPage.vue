cb71e911c3ddf991a9c1981010f91697354f899d
Egor Kroshkin <kroshkin.egor@yandex.ru>
2022-10-21 04:16:27 +0300

    add cards in advantages

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,16 +1,31 @@
 <template>
-    <div class="hero">
-        <div class="hero__wrapper wrapper">
-            <h1 class="hero__heading">Тренируйся <br/> самостоятельно</h1>
-            <p class="hero__description">Составь свой персонализированный план <br/> спортивных и фитнес тренировок</p>
-        </div>
+    <div>
+        <section class="hero">
+            <div class="hero__wrapper wrapper">
+                <h1 class="hero__heading">Тренируйся <br/> самостоятельно</h1>
+                <p class="hero__description">Составь свой персонализированный план <br/> спортивных и фитнес тренировок</p>
+            </div>
+        </section>
+
+        <section class="advantages">
+            <div class="advantages__wrapper wrapper">
+                <h1 class="advantages__heading">Преимущества <br> онлайн тренировок</h1>
+                <div class="advantages__list">
+                    <AdvantagesCard></AdvantagesCard>
+                </div>
+            </div>
+        </section>
     </div>
+    
     
 </template>
 
 <script>
+    import AdvantagesCard from '@/components/AdvantagesCard'
     export default {
-        
+        components: {
+            AdvantagesCard
+        }
     }
 </script>
 
@@ -34,6 +49,14 @@
         font-size: 24px;
         line-height: 130%;      
         color: #F6F6F6;
+    }
+
+    .advantages__heading {
+        font-weight: 500;
+        font-size: 56px;
+        text-align: center;
+        margin-top: 100px;
+        margin-bottom: 70px;
     }
     
 </style>
