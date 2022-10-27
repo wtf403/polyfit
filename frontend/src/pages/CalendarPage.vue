6d65b08ec18cc1b916f46ef6a5bd219f758f2887
Andrew Kiverin <kiverin03@yandex.ru>
2022-10-27 17:38:40 +0300

    fix line-height

--- frontend/src/pages/CalendarPage.vue
+++ frontend/src/pages/CalendarPage.vue
@@ -1,36 +1,35 @@
 <template>
-    <div>
-        <h1>
-            vdfbvzdfb
+  <div>
+    <h1>
+      vdfbvzdfb
+    </h1>
+    <h1>
+      vdfbvzdfb
+    </h1>
+    <h1>
+      vdfbvzdfb
+    </h1>
+    <div class="hero">
+      <div class="hero__wrapper wrapper">
+        <h1 class="hero__heading">
+          Занимайся <br> планированием
         </h1>
-        <h1>
-            vdfbvzdfb
-        </h1>
-        <h1>
-            vdfbvzdfb
-        </h1>
-        <div class="hero">
-            <div class="hero__wrapper wrapper">
-                <h1 class="hero__heading">
-                    Занимайся <br> планированием
-                </h1>
-                <p class="hero__description">
-                    Создай расписание занятий и под строй <br> под свой распорядок дня.
-                </p>
-            </div>
-        </div>
-        
+        <p class="hero__description">
+          Создай расписание занятий и под строй <br> под свой распорядок дня.
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+
+};
 </script>
-    
+
 <style  scoped>
-    .hero__wrapper {
+  .hero__wrapper {
     height: 750px;
     padding-left: 40px;
     background-image: url('../assets/hero cover (1).png');
@@ -38,11 +37,11 @@
   }
 
   .hero__heading {
-    font-weight: 700;
-    font-size: 64px;
-    line-height: 110%;
     padding-top: 458px;
     padding-left: 40px;
+    font-weight: 700;
+    font-size: 64px;
+    line-height: 1.1;
   }
 
   .hero__description {
@@ -50,7 +49,7 @@
     padding-left: 40px;
     font-weight: 400;
     font-size: 24px;
-    line-height: 130%;
+    line-height: 1.3;
   }
 
 </style>
