3a3d71b9262c9c3af4b25da933f68616109a04b0
Egor Kroshkin <kroshkin.egor@yandex.ru>
2022-10-31 09:30:34 +0300

    add-main-content-to-player

---
+++ frontend/src/pages/WorkoutPlayerPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="player">
+    <div class="player__wrapper wrapper">
+      <div class="player__content">
+        <WorkoutPlayerInfo />
+        <WorkoutPlayerSwitch />
+        <img :src="require('@/assets/player-img.png')" alt="player-img" class="player__img">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import WorkoutPlayerInfo from '@/components/WorkoutPlayerInfo';
+import WorkoutPlayerSwitch from '@/components/WorkoutPlayerSwitch';
+export default {
+  components: {
+    WorkoutPlayerInfo, WorkoutPlayerSwitch,
+  },
+};
+</script>
+
+<style scoped>
+.player__wrapper {
+  position: relative;
+  padding-top: 60px;
+}
+
+.player__content {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 40px;
+}
+
+.player__img {
+  position: absolute;
+  z-index: 0;
+}
+
+
+</style>
