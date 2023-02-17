5f6ef5a48b64dc754dd3546500473ebdae08de1e
Andrew Kiverin <kiverin03@yandex.ru>
2023-02-17 17:14:22 +0300

    add vuex and api workouts and other changes

---
+++ frontend/src/components/TheExercise.vue
@@ -0,0 +1,80 @@
+<template>
+  <router-link :to="{ path: '/exercises/'+exercise.id, query: {item: exercise}}" class="exercises-content__exercises-card exercises-card">
+    <img :src="exercise.exerciseImage" alt="Image for exercise" class="exercises__image">
+    <div class="exercises__info">
+      <h4 class="exercises__name">
+        {{exercise.exerciseName}}
+      </h4>
+    </div>
+    <p class="exercises__count">
+      {{exercise.exerciseCount}}
+    </p>
+  </router-link>
+</template>
+
+<script>
+
+export default {
+  name: 'WorkoutsItemComponent',
+  props: ['exercise'],
+  data() {
+    return {
+      // exercise: this.$route.query;
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.exercises-card {
+  position: relative;
+}
+.exercises__image {
+  width: 250px;
+  height: 250px;
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.exercises__info {
+  position: absolute;
+  padding: 20px;
+  background-color: #00000080;
+  backdrop-filter: blur(4px);
+  border-radius: 0 0 8px 8px;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 12px;
+  min-height: 80px;
+  justify-content: center;
+}
+
+.exercises__name {
+  display: -webkit-box;
+  max-height: 48px;
+  overflow: hidden;
+  font-weight: 400;
+  font-size: 18px;
+  color: white;
+  line-height: 1;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+.exercises__count {
+  position: absolute;
+  padding: 8px 12px;
+  color: #f66c1e;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1;
+  background-color: #00000080;
+  border-radius: 8px;
+  bottom: 216px;
+  right: 8px;
+}
+</style>
--- frontend/src/pages/AdminPage.vue
+++ frontend/src/pages/AdminPage.vue
@@ -9,7 +9,7 @@
           <button class="admin__add" @click="CreateWorkout()">
             Создать тренировку
           </button>
-          <input v-model="search" type="search" class="admin__search" placeholder="Поиск тренировки">
+          <input v-model="searchWorkout" type="search" class="admin__search" placeholder="Поиск тренировки">
         </div>
         <ul class="admin__workouts">
           <li v-for="(workout, index) in sortWorkouts" :key="index" class="admin__workout">
@@ -19,6 +19,30 @@
                 Изменить
               </button>
               <button class="admin__delete" @click="DeleteWorkout(workout)">
+                Удалить
+              </button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="admin__block">
+        <h2 class="admin__title">
+          Упражнения
+        </h2>
+        <div class="admin__head-workouts">
+          <button class="admin__add" @click="CreateWorkout()">
+            Создать упражнение
+          </button>
+          <input v-model="searchExercise" type="search" class="admin__search" placeholder="Поиск упражнения">
+        </div>
+        <ul class="admin__workouts">
+          <li v-for="(exercise, index) in sortExercise" :key="index" class="admin__workout">
+            <TheExercise :exercise="exercise" />
+            <div class="admin__actions">
+              <button class="admin__change" @click="ChangeWorkout(exercise)">
+                Изменить
+              </button>
+              <button class="admin__delete" @click="DeleteWorkout(exercise)">
                 Удалить
               </button>
             </div>
@@ -43,6 +67,7 @@ import WorkoutsItem from '@/components/WorkoutsItem';
 import AdminPopupChange from '@/components/AdminPopupChange';
 import AdminPopupDelete from '@/components/AdminPopupDelete.vue';
 import AdminPopupCreate from '@/components/AdminPopupCreate.vue';
+import TheExercise from '@/components/TheExercise.vue';
 
 export default {
   components: {
@@ -50,11 +75,14 @@ export default {
     AdminPopupChange,
     AdminPopupDelete,
     AdminPopupCreate,
+    TheExercise,
   },
   data() {
     return {
-      search: '',
+      searchWorkout: '',
       selectWorkout: {},
+      searchExercise: '',
+      selectExercise: {},
       workouts: [{
         image: require('@/assets/WorkoutImage-0.png'),
         name: 'Работа над резервной копией',
@@ -249,21 +277,109 @@ export default {
         desc: 'Данная тренировка расчитана на улучшение выносливости тела к умеренным физическим нагрузкам. Время, выделенное на выполнение упражнений соответсвует уровню сложности данной тренировки.',
         inventory: 'Гантели, спортивный коврик.',
       }],
+      exercises: [
+        {
+          exerciseName: 'Подъем на носки',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/001.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Приседания',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/002.webp',
+          exerciseCount: '20 повторений',
+        },
+        {
+          exerciseName: 'Пружинка в приседе нонстоп',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/004x002.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Высокая планка',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/150.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Колени в пол в планке на локтях',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/157.webp',
+          exerciseCount: '12 повторений',
+        },
+        {
+          exerciseName: 'Руки-локти в планке',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/158.webp',
+          exerciseCount: '12 повторений',
+        },
+        {
+          exerciseName: 'Жим от плеч двумя руками',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/196.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Пружинка в гиперэкстензии',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/226x225.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Разведение гантелей лежа на 2 счета',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/250.webp',
+          exerciseCount: '40 секунд',
+        },
+        {
+          exerciseName: 'Жим узкий- французский поочередно на 2 счета',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/262.webp',
+          exerciseCount: '1 минута 20 секунд',
+        },
+        {
+          exerciseName: 'Опускание согнутых ног по одной',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/276.webp',
+          exerciseCount: '18 повторений',
+        },
+        {
+          exerciseName: 'Пружинка скручивания руки за головой',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/278x277.webp',
+          exerciseCount: '16 повторений',
+        },
+        {
+          exerciseName: 'Ситапы',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/285.webp',
+          exerciseCount: '20 повторений',
+        },
+        {
+          exerciseName: 'Крисс-кросс сидя колено-локоть',
+          exerciseImage: 'https://static.w.f-365.ru/imported/webp/8/292.webp',
+          exerciseCount: '24 повторений',
+        },
+      ],
     };
   },
   computed: {
+    allWorkouts() {
+      return this.$store.getters.allWorkouts;
+    },
     sortWorkouts() {
-      let text = this.search;
+      let text = this.searchWorkout;
       let list = this.workouts;
       list = this.sortBySearch(list, text);
       return list;
     },
+    sortExercise() {
+      let text = this.searchExercise;
+      let list = this.exercises;
+      list = this.sortBySearchExercise(list, text);
+      return list;
+    },
+  },
+  async mounted() {
+    this.$store.dispatch('fetchWorkouts');
   },
   methods: {
     sortBySearch(list, text) {
-      // return list.filter(function(item) { return item.difficulty === 'сложно'; });
       return list.filter(function(item) {
-        return (item.name.toLowerCase().includes(text.toLowerCase()) & item.desc.toLowerCase().includes(text.toLowerCase()));
+        return (item.name.toLowerCase().includes(text.toLowerCase()) || item.desc.toLowerCase().includes(text.toLowerCase()));
+      });
+    },
+    sortBySearchExercise(list, text) {
+      return list.filter(function(item) {
+        return (item.exerciseName.toLowerCase().includes(text.toLowerCase()) || item.exerciseCount.toLowerCase().includes(text.toLowerCase()));
       });
     },
     ChangeWorkout(obj) {
